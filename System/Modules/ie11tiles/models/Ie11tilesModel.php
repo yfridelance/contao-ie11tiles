960a6e9d7fc6cf18009531e0c242a6d0c6b8446d
Yves Fridelance <yves.fridelance@me.com>
2014-10-15 00:19:23 +0200

    inital

---
+++ System/Modules/ie11tiles/models/Ie11tilesModel.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package   ie11tiles
+ * @author    Yves Fridelance <yves@fridelance.ch>
+ * @license   GNU/LGPL
+ * @copyright Fridelance 2014
+ */
+
+
+/**
+ * Run in a custom namespace, so the class can be replaced
+ */
+namespace Contao;
+
+
+/**
+ * Class Ie11tilesModel
+ *
+ * @copyright  Fridelance 2014
+ * @author     Yves Fridelance <yves@fridelance.ch>
+ * @package    ie11tiles
+ */
+class Ie11tilesModel extends \Model
+{
+
+
+    /**
+     * Table name
+     * @var string
+     */
+    protected static $strTable = 'tl_ie11tiles';
+}
