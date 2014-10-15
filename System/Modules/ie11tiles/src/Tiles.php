2a65ecaceabdb8bc08e743d8416b100569cee162
yves <yves@fridelance.ch>
2014-10-15 12:24:39 +0200

    inital

--- System/Modules/ie11tiles/src/Tiles.php
+++ System/Modules/ie11tiles/src/Tiles.php
@@ -30,6 +30,6 @@ class Tiles
 
     public function test()
     {
-        \Ie11tiles\Helper::writeFile('test.xml', 'Dies ist ein test');
+        \Fry\Ie11tiles\Helper::writeFile('test.xml', 'Dies ist ein test');
     }
 } 
